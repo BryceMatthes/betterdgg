f85a2c6a18573bdadf50ebc88916ba7138f12e2a
PurpleCox <Christopherflinn@hotmail.com>
2016-08-03 04:59:33 +0100

    3 hours and 45 minutes of my life wasted on testing, and working out what was needed, for 3 lines of code. wew lad.

--- betterdgg/modules/mentions.js
+++ betterdgg/modules/mentions.js
@@ -7,9 +7,12 @@
                 gui.lines.on('mousedown', 'div.user-msg a.user', function() {
                     if (bdgg.settings.get('bdgg_highlight_selected_mentions')) {
                         var username = $(this).text();
-                        gui.lines.find(':contains(' + username + ')').addClass('focused');
+                        var users = [$(this).parents().parents().find(".chat-user:contains(" + username + ")")];
+                        for (var i = 0; i < $(users)[0].length - 1; i++) {
+                            window.destiny.chat.gui.stylesheet.insertRule('.user-msg[data-username="' + ($($(users)[0].parent().parent()[i]).find(".user").text().toLowerCase()) + '"]{opacity:1 !important;}', window.destiny.chat.gui.stylesheet.cssRules.length);
+                            window.destiny.chat.gui.focusedUsers.push(($($(users)[0].parent().parent()[i]).find(".user").text().toLowerCase()));
+                        }
                     }
-
                     return false;
                 });
             }
